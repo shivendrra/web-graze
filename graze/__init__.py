a415eacb9ac07dd55d0af008d4d5261a715bf239
Shivendra <shivharsh44@gmail.com>
2024-07-26 14:19:32 +0530

    built an image scrapper

--- graze/__init__.py
+++ graze/__init__.py
@@ -1,3 +1,4 @@
 from .youtube.base import Youtube as youtube
 from .britannica.main import Britannica as britannica
 from .wikipedia.main import WikiScraper as wikipedia
+from .unsplash import unsplash
